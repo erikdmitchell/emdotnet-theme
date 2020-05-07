8725a07ba78e0a44345d8d74a4d93b17abbea861
Erik <erikdmitchell@gmail.com>
2020-05-07 12:06:17 -0400

    tweaked home about

--- front-page.php
+++ front-page.php
@@ -34,20 +34,9 @@
     </div>
 
     <div class="home-about">
-        <div class="container">
-            <div class="col-md-12">
-                <?php
-                if ( have_posts() ) :
-                    while ( have_posts() ) :
-                        the_post();
-                        ?>
-                        <?php the_content(); ?>
-                        <?php
-                endwhile;
-endif;
-                ?>
-            </div>
-        </div>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
     </div>
 
 <?php
