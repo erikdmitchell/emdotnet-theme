4eaca06f4b08e45354b951832385fc6f0be0fa3d
Erik <erikdmitchell@gmail.com>
2020-05-08 14:29:33 -0400

    updated social media icons

--- footer.php
+++ footer.php
@@ -5,11 +5,11 @@
                 <div class="footer-widget footer-widget-3">
                     <h3>Connect with Me</h3>
                     <ul class="social-media">
-                        <li id="social-media-facebook"><a href="https://www.facebook.com/erikdmitchell"><i class="fa fa-facebook-square"></i></a></li>
-                        <li id="social-media-twitter"><a href="https://twitter.com/erikdmitchell"><i class="fa fa-twitter-square"></i></a></li>
-                        <li id="social-media-instagram"><a href="https://instagram.com/erikdmitchell"><i class="fa fa-instagram"></i></a></li>
-                        <li id="social-media-linkedin"><a href="https://www.linkedin.com/in/erikdmitchell"><i class="fa fa-linkedin-square"></i></a></li>
-                        <li id="social-media-github"><a href="https://github.com/erikdmitchell"><i class="fa fa-github"></i></a></li>
+                        <li id="social-media-facebook"><a href="https://www.facebook.com/erikdmitchell"><i class="fab fa-facebook"></i></a></li>
+                        <li id="social-media-twitter"><a href="https://twitter.com/erikdmitchell"><i class="fab fa-twitter"></i></a></li>
+                        <li id="social-media-instagram"><a href="https://instagram.com/erikdmitchell"><i class="fab fa-instagram"></i></a></li>
+                        <li id="social-media-linkedin"><a href="https://www.linkedin.com/in/erikdmitchell"><i class="fab fa-linkedin"></i></a></li>
+                        <li id="social-media-github"><a href="https://github.com/erikdmitchell"><i class="fab fa-github"></i></a></li>
                     </ul>
                 </div>
             </div><!-- .footer-widgets -->
