67484dd7beefa487ee7498c073cde2d0d14cd279
Erik <erikdmitchell@gmail.com>
2020-05-13 10:54:17 -0400

    updated links color and tags format

--- content.php
+++ content.php
@@ -57,5 +57,5 @@
     </div><!-- .entry-content -->
     <?php endif; ?>
 
-    <?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+    <?php the_tags( '<div class="entry-meta">Tags: <span class="tag-links">', ', ', '</span></div>' ); ?>
 </article><!-- #post-## -->
---
+++ single-plugins.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<div class="page-header">
+    <div class="em-container">
+        <div class="em-row">
+            <div class="em-col">
+                <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="em-container">
+    <div class="em-row project-thumb-wrap">
+        <div class="em-col">
+            <?php emdotnet_theme_post_thumbnail( 'project-single-thumb' ); ?>
+        </div>
+    </div>
+    
+    <div class="em-row">
+        <div class="em-col">
+            <?php
+            // Start the Loop.
+            while ( have_posts() ) :
+                the_post();
+                get_template_part( 'content' );
+
+                // Previous/next post navigation.
+                emdotnet_theme_post_nav();
+            endwhile;
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
+
