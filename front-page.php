a25af28c8a7adba82003f766ae44397e92ccd1b4
Erik <erikdmitchell@gmail.com>
2020-11-19 09:18:29 -0500

    removed hardcode

--- front-page.php
+++ front-page.php
@@ -9,28 +9,6 @@
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cobbles.jpg" />
     </div>
 
-    <div class="tools-i-use">
-        <div class="container">
-            <div class="row">
-                <div class="col-3 tools-block">
-                    <div class="icon"><i class="fab fa-wordpress"></i></div>
-                    <h2 class="title">WordPress</h2>
-                    <p>Currently powering 25% of the web, WordPress is web software that can be used to create beautiful and powerful websites, apps and virtually anything you can imagine.</p>
-                </div>
-                <div class="col-3 tools-block">
-                    <div class="icon"><i class="fa fa-code"></i></div>
-                    <h2 class="title">Development</h2>
-                    <p>The three core "languages" I use for development are PHP, JavaScript and CSS. I leverage these languages to create powerful experiences that are easy to manage and grow.</p>
-                </div>
-                <div class="col-3 tools-block">
-                    <div class="icon"><i class="fa fa-picture-o"></i></div>
-                    <h2 class="title">Design/UX</h2>
-                    <p> I utilize tools such as react, jQuery, node and Sass to maximize performance and create beautiful websites/applications that also deliver a powerful user experience.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="home-about">
         <div class="container">
             <?php
