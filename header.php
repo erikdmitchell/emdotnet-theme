bc264720c4f262fc6b4ef090102ee69fbacbe5e6
Erik <erikdmitchell@gmail.com>
2021-01-22 12:57:03 -0500

    added ga tracking

--- header.php
+++ header.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9588856-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+    
+        gtag('config', 'UA-9588856-1');
+    </script>
+
     <?php wp_head(); ?>
 </head>
 
